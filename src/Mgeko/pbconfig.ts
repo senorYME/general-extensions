e1cf9cff4728cea8852f6df6723f0c9b621ea2a0
Eduard Smet <contact@celarye.dev>
2024-11-30 16:18:52 +0100

    chore: Mgeko version bump

--- src/Mgeko/pbconfig.ts
+++ src/Mgeko/pbconfig.ts
@@ -3,7 +3,7 @@ import { ContentRating, SourceIntents } from "@paperback/types";
 export default {
   icon: "icon.png",
   name: "Mgeko",
-  version: "1.0.0",
+  version: "1.0.1",
   description:
     "The mgeko.cc (old domains: mcreader.net, manga-raw.club) extension.",
   contentRating: ContentRating.MATURE,
