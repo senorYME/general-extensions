1fe4815d469148232cca0a6e8c9fc2e21909b761
YME <98798167+senorYME@users.noreply.github.com>
2025-02-15 01:03:13 -0800

    Update and rename ManganeloInterceptor.ts to ManganatoInterceptor.ts

--- src/Manganato/ManganatoInterceptor.ts
+++ src/Manganato/ManganatoInterceptor.ts
@@ -1,10 +1,10 @@
 import { PaperbackInterceptor, Request, Response } from "@paperback/types";
 
-export class NeloInterceptor extends PaperbackInterceptor {
+export class NatoInterceptor extends PaperbackInterceptor {
   override async interceptRequest(request: Request): Promise<Request> {
     request.headers = {
       ...request.headers,
-      referer: `https://m.manganelo.com`,
+      referer: `https://manganato.com`,
       "user-agent": await Application.getDefaultUserAgent(),
     };
     return request;
